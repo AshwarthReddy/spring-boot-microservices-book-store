28b284d72a1d314b2b287b32d16947cd49ce9cdd
aswarthana <banr20@gmail.com>
2024-05-10 23:07:57 +0400

    intial commit

---
+++ booklist-service/src/test/java/com/anr/booklistservice/BookListServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anr.booklistservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookListServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
