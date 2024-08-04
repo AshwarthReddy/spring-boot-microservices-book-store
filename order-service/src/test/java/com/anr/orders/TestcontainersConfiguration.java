e742fc7fa760bcbbc670b1b8394828f4009d1d40
aswarthana <banr20@gmail.com>
2024-08-04 23:02:17 +0400

    created the new order-service micro-service

---
+++ order-service/src/test/java/com/anr/orders/TestcontainersConfiguration.java
@@ -0,0 +1,25 @@
+package com.anr.orders;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.RabbitMQContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+    @Bean
+    @ServiceConnection
+    MySQLContainer<?> mysqlContainer() {
+        return new MySQLContainer<>(DockerImageName.parse("postgres:16-alpine"));
+    }
+
+    @Bean
+    @ServiceConnection
+    RabbitMQContainer rabbitMQContainer(){
+        return new RabbitMQContainer(DockerImageName.parse("rabbitmq-3.12.11-alpine"));
+    }
+
+}
