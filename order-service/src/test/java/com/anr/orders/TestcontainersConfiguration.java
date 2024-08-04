f7d14cfa69177198b34af5d394db06db3f50ab93
aswarthana <banr20@gmail.com>
2024-08-04 23:10:32 +0400

    test case fixes

--- order-service/src/test/java/com/anr/orders/TestcontainersConfiguration.java
+++ order-service/src/test/java/com/anr/orders/TestcontainersConfiguration.java
@@ -3,7 +3,7 @@ package com.anr.orders;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.RabbitMQContainer;
 import org.testcontainers.utility.DockerImageName;
 
@@ -12,13 +12,13 @@ class TestcontainersConfiguration {
 
     @Bean
     @ServiceConnection
-    MySQLContainer<?> mysqlContainer() {
-        return new MySQLContainer<>(DockerImageName.parse("postgres:16-alpine"));
+    PostgreSQLContainer<?> mysqlContainer() {
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:16-alpine"));
     }
 
     @Bean
     @ServiceConnection
     RabbitMQContainer rabbitMQContainer() {
-        return new RabbitMQContainer(DockerImageName.parse("rabbitmq-3.12.11-alpine"));
+        return new RabbitMQContainer(DockerImageName.parse("rabbitmq:3.12.11-alpine"));
     }
 }
