dde1546a77e361e6f4ddf6bb775465546d3907b3
aswarthana <banr20@gmail.com>
2024-08-05 20:06:22 +0400

    implemented the shed locking for multipul instances run

--- order-service/src/main/java/com/anr/orders/jobs/OrderEventsPublishingJob.java
+++ order-service/src/main/java/com/anr/orders/jobs/OrderEventsPublishingJob.java
@@ -3,6 +3,7 @@ package com.anr.orders.jobs;
 import com.anr.orders.domain.OrderEventService;
 import java.time.Instant;
 import lombok.extern.slf4j.Slf4j;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -17,6 +18,7 @@ class OrderEventsPublishingJob {
     }
 
     @Scheduled(cron = "${orders.publish-order-events-job-cron}")
+    @SchedulerLock(name = "publishOrderEvents")
     public void publishOrderEvents() {
         log.info("Publishing Order Events at {}", Instant.now());
         orderEventService.publishOrderEvents();
--- order-service/src/main/java/com/anr/orders/jobs/OrderProcessingJob.java
+++ order-service/src/main/java/com/anr/orders/jobs/OrderProcessingJob.java
@@ -3,6 +3,7 @@ package com.anr.orders.jobs;
 import com.anr.orders.domain.OrderService;
 import java.time.Instant;
 import lombok.extern.slf4j.Slf4j;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -17,6 +18,7 @@ class OrderProcessingJob {
     }
 
     @Scheduled(cron = "${orders.new-orders-job-cron}")
+    @SchedulerLock(name = "processNewOrders")
     public void processNewOrders() {
         log.info("Processing new orders at {}", Instant.now());
         orderService.processNewOrders();
