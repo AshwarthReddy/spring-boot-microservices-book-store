f2e08376065cec785c7f6c439a6c8668a3cf1e01
aswarthana <banr20@gmail.com>
2024-11-14 18:38:55 +0400

    fixes

--- api-gateway-service/src/main/java/com/anr/apigateway/apigateway/ApiGatewayApplication.java
+++ api-gateway-service/src/main/java/com/anr/apigateway/apigateway/ApiGatewayApplication.java
@@ -2,11 +2,7 @@ package com.anr.apigateway.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @RestController
@@ -16,8 +12,8 @@ public class ApiGatewayApplication {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
-    @GetMapping(value = "/token")
-    public Mono<String> getHome(@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient) {
-        return Mono.just(authorizedClient.getAccessToken().getTokenValue());
-    }
+    //    @GetMapping(value = "/token")
+    //    public Mono<String> getHome(@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient) {
+    //        return Mono.just(authorizedClient.getAccessToken().getTokenValue());
+    //    }
 }
